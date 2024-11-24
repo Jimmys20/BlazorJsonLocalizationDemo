3c0ec9da373af583cb2696a481f15cc4a234d878
jimmys20 <maragkosd12@gmail.com>
2024-11-24 14:56:56 +0200

    Add project files.

---
+++ BlazorJsonLocalizationDemo/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SoloX.BlazorJsonLocalization.WebAssembly;
+
+namespace BlazorJsonLocalizationDemo
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddWebAssemblyJsonLocalization(builder =>
+            {
+                builder
+                  .EnableDisplayKeysWhileLoadingAsynchronously()
+                  .UseHttpHostedJson(options =>
+                  {
+                      options.ApplicationAssembly = typeof(Program).Assembly;
+                      options.ResourcesPath = "Resources";
+                  });
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
